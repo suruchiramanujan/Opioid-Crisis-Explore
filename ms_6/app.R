f6ffd7aa29d9af91efd506f51fae27b376df58d9
“suruchiramanujan” <you@example.com>
2020-04-02 18:24:34 -0500

    moved drug overdose csv but graph still not showing

---
+++ ms_6/app.R
@@ -0,0 +1,47 @@
+library(shiny)
+library(tidyverse)
+library(gt)
+library(dplyr)
+library(ggplot2)
+library(lubridate)
+
+### Read in Data
+drugoverdosedeaths<- read.csv("VSRR_Provisional_Drug_Overdose_Death_Counts.csv")
+
+drugoverdosedeaths <- drugoverdosedeaths %>%
+    clean_names() %>%
+    select("state", "year", "month", "indicator", 
+           "data_value", "state_name", "predicted_value") %>%
+    filter(str_detect(indicator, "Drug Overdose Deaths")) %>%
+    group_by("state_name") %>%
+    mutate(time = paste(month, year, sep = " ")) %>%
+    mutate(time=mdy(time)) %>%
+    arrange(time)
+drugoverdosedeaths$data_value <-as.numeric(drugoverdosedeaths$data_value)
+
+ui <- fluidPage(
+    titlePanel("Drug Overdoses by State Over Time"),
+    sliderInput("time", "Time Slider", min(drugoverdosedeaths$time),
+    max(drugoverdosedeaths$time),
+              value = min(drugoverdosedeaths$time)),
+    plotOutput("overdose_counts")
+)
+
+
+server <- function(input, output, session) {
+    #Rationing Map 
+    output$overdose_counts <- renderPlot({
+        ggplot(drugoverdosedeaths, aes(x= reorder(state_name,-data_value), y= data_value)) +
+            geom_col() +
+            facet_wrap(~time)
+            labs(title = "Drug Overdose Deaths by State Over Time", 
+                 x = "State", 
+                 y = "Drug Overdose Deaths") +
+            
+            theme_classic()+ 
+            
+            theme(axis.text.x = element_text(angle = 45, hjust = 1))
+    })
+    
+}
+shinyApp(ui, server)
